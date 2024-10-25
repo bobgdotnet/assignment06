334b312d24426ce42aaed3da47baa7335a2b3880
bobgdotnet <95641422+bobgdotnet@users.noreply.github.com>
2024-10-25 08:56:21 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // I got it down to one line by using a "," 
-window.onload = () => empForm.onsubmit = e => (e.preventDefault(), [...e].forEach(element => element.id && console.log(`${element.id}: ${element.value}`)));
+window.onload = () => empForm.onsubmit = e => (e.preventDefault(), [...e.target.elements].forEach(element => element.id && console.log(`${element.id}: ${element.value}`)));
 //load data from the form     when submit is pressed, skip default, then  go through the array and log the data/value pair.
 
 // 3rd attempt: I realized I could use the names AND values and not declare any variables
